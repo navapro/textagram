0e39238e1373f6c9507c6c33000a50e4069a3eca
Naveen <navaneethk002@gmail.com>
2022-11-21 22:26:06 -0600

    created all

--- textagram_ai/api.py
+++ textagram_ai/api.py
@@ -7,6 +7,27 @@ from io import BytesIO
 from firebase_upload import upload_to_firebase
 from test import esrgan
 import json
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
+
 
 app = FastAPI()
 
@@ -22,8 +43,12 @@ model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, revision="fp16", use_auth_token=auth_token)
 pipe = pipe.to("cuda")
 
-@app.get("/")
-def generate(prompt: str): 
+class RequestBody(BaseModel):
+    prompt: str
+
+@app.post("/", dependencies=[Depends(api_key_auth)])
+async def generate(body: RequestBody): 
+    prompt = body.prompt
     with torch.cuda.amp.autocast(True): 
         image = pipe(prompt, guidance_scale=8.5).images[0]
 
